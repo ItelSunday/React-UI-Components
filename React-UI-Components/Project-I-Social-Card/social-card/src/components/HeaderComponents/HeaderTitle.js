6e103c4ac74d4a972e0caf327bbeee64c147092f
ItelSunday <iteldomingotech@gmail.com>
2019-02-12 06:47:32 -0800

    mvp complete

--- React-UI-Components/Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ React-UI-Components/Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+import moment from "moment";
+
+function HeaderTitle() {
+    return <div className="header-title">
+              <h2 className="header">Lamda School</h2>
+              <p className="sub-header">@lamdaschool &#183; {moment().format("Do MMM ")}</p>
+
+    </div>;
+  }
+
+  export default HeaderTitle;
