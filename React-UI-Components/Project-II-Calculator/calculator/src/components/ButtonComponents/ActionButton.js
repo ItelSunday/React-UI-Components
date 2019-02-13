41959f3ed1319357c7db5344131256567eaa27dc
ItelSunday <iteldomingotech@gmail.com>
2019-02-12 17:44:34 -0800

    updated layout

--- React-UI-Components/Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ React-UI-Components/Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+             <div className="action-btn">
+                <ActionButton className="clear">clear</ActionButton>
+            </div>
+
+           
+    );
+};
+
+export default ActionButton;
--- React-UI-Components/Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ React-UI-Components/Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = props => {
+    return (
+            <div className="screen">
+                <button className="display"></button>
+            </div>
+
+    );
+};
+
+export default CalculatorDisplay;
